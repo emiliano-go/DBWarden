56e4dcc93fe24c6b996d2dbef8b7a493053cb278
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-02-07 23:32:20 -0300

    Config

---
+++ dbwarden/constants.py
@@ -0,0 +1,12 @@
+from typing import Final
+
+MIGRATIONS_DIR: Final[str] = "migrations"
+ENV_FILE: Final[str] = ".env"
+RUNS_ALWAYS_FILE_PREFIX: Final[str] = "RA__"
+RUNS_ON_CHANGE_FILE_PREFIX: Final[str] = "ROC__"
+VERSION_FILE_PREFIX: Final[str] = "V"
+DEFAULT_DELIMITER: Final[str] = ";"
+
+DBWARDEN_VERSION: Final[str] = "1.0.0"
+
+LOG_FORMAT: Final[str] = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
